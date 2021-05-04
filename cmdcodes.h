7bfa1fc57d018e1486aa8f6a9a6e42a1c27c61d6
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-04 20:12:16 +0530

    final cmdcodes.h
    Define the custom commands being used on the CLI

---
+++ cmdcodes.h
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cmdcodes.h
+ *
+ *    Description:  This file Comtains all CMD Codes for commands
+ *
+ *        Version:  1.0
+ *       Compiler:  gcc
+ *
+ *         Author: Parth Arora
+ *        Batch: 2CS09
+ *        
+ *
+ * =====================================================================================
+ */
+
+#ifndef __CMDCODES__
+#define __CMDCODES__
+
+#define CMDCODE_SHOW_NW_TOPOLOGY    1   /*show topology*/
+#define CMDCODE_PING                2   /*run <node-name> ping <ip-address>*/
+#define CMDCODE_SHOW_NODE_ARP_TABLE 3   /*show node <node-name> arp*/
+#define CMDCODE_RUN_ARP             4   /*run node <node-name> resolve-arp <ip-address>*/
+#define CMDCODE_INTF_CONFIG_L2_MODE 5   /*config node <node-name> interface <intf-name> l2mode <access|trunk>*/
+#define CMDCODE_INTF_CONFIG_IP_ADDR 6   /*config node <node-name> interface <intf-name> ip-address <ip-address> <mask>*/
+#define CMDCODE_INTF_CONFIG_VLAN    7   /*config node <node-name> interface <intf-name> vlan <vlan-id>*/
+#define CMDCODE_SHOW_NODE_MAC_TABLE 8   /*show node <node-name> mac*/
+#define CMDCODE_SHOW_NODE_RT_TABLE  9   /*show node <node-name> rt*/
+#define CMDCODE_CONF_NODE_L3ROUTE   10  /*config node <node-name> route <ip-address> <mask> [<gw-ip> <oif>]*/
+#define CMDCODE_ERO_PING            11  /*run <node-name> ping <ip-address> ero <ero-ip-address>*/
+#endif /* __CMDCODES__ */
